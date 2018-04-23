1acd1640e639410f34b75ba44ce2fb42d9c16ff3
Bekzatsky <mn.bekzat@gmail.com>
2018-04-23 17:59:19 +0600

    added @CrossOrigin annotation for frontend access

--- src/main/java/kz/restservice/restapp/controller/UserController.java
+++ src/main/java/kz/restservice/restapp/controller/UserController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ser.std.DateSerializer;
 import kz.restservice.restapp.model.User;
 import kz.restservice.restapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8081", allowedHeaders = "*")
 public class UserController {
 
     @Autowired
